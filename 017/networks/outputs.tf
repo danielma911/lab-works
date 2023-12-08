411b3883d6da4332c48e29e0c502ae2513af61ac
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2023-12-08 17:42:50 -0500

    update 017

--- 017/networks/outputs.tf
+++ 017/networks/outputs.tf
@@ -2,18 +2,6 @@
 # Outputs
 # ------------------------------------------------------------------------------------
 
-output "subnet_name_mgmt" {
-  value = module.vpc_mgmt.subnets_names[0]
-}
-
-output "subnet_name_untrust" {
-  value = module.vpc_untrust.subnets_names[0]
-}
-
-output "subnet_name_trust" {
-  value = module.vpc_trust.subnets_names[0]
-}
-
 output "trust_subnet_gateway" {
   value = data.google_compute_subnetwork.trust.gateway_address
 }
