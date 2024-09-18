05a11b26ac41850dc7547a1bd5df2a8d460f2a91
Matt McLimans <mmclimans@paloaltonetworks.com>
2024-09-18 08:29:08 -0400

    Update outputs.tf

--- 012/terraform/outputs.tf
+++ 012/terraform/outputs.tf
@@ -12,3 +12,17 @@ output "gemini_app" {
 output "openai_app" {
   value = "http://${google_compute_instance.ai.network_interface[0].access_config[0].nat_ip}:80"
 }
+
+
+output "SET_ENV_VARS" {
+  value = google_compute_instance.ai.zone
+}
+
+
+output "SET_ENV_VARS" {
+  value = <<EOF
+export PROJECT_ID=${local.project_id}
+export REGION=${local.region}
+export ZONE=${google_compute_instance.ai.zone}
+EOF
+}
