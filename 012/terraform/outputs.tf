c2150274a7293d63095f154aa4abd7d6c5bf329b
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2024-09-19 09:58:41 -0400

    Update outputs.tf

--- 012/terraform/outputs.tf
+++ 012/terraform/outputs.tf
@@ -13,12 +13,6 @@ output "openai_app" {
   value = "http://${google_compute_instance.ai.network_interface[0].access_config[0].nat_ip}:80"
 }
 
-
-output "SET_ENV_VARS" {
-  value = google_compute_instance.ai.zone
-}
-
-
 output "SET_ENV_VARS" {
   value = <<EOF
 export PROJECT_ID=${local.project_id}
